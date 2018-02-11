90f99309be42cdd819ff9adb2493005deec7401d
Mags12 <33438630+Mags12@users.noreply.github.com>
2018-02-11 10:50:13 -0500

    Add files via upload
    Updated javascript file to pull the first two dates from the Alpa Vantage JSON file

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const request = require('request-promise');
-
-const apiKey = require('./api_key');
+const apiKey = ('/.apikey.js');
 const app = express();
 
 app.use(logger('dev'));
@@ -32,10 +31,47 @@ app.get('/:symbol', function(req, res) {
     getDailyTimeSeries(symbol)
         .then(function(stockData) {
             const obj = stockData['Time Series (Daily)'];
-            console.log(obj);
+            return obj; 
         })
+        .then(function(data){
+            stockArray(data)
+        });
 });
 
+function stockArray(data){
+    //Formatting date
+    const date =new Date();        
+    let dd = date.getDate();
+    let cc = date.getDate()-1;
+    let sun = date.getDate()-2; 
+    let mon = date.getDate()-3;
+    let mm = date.getMonth()+1; 
+    const yyyy = date.getFullYear(); 
+    if(dd<10){ dd='0'+dd}; 
+    if(cc<10){ cc='0'+cc};
+    if(mm<10){ mm='0'+mm};
+    if(sun<10){sun='0'+sun}
+    if(mon<10){ mon='0'+mon};
+    const today = yyyy+'-'+mm+'-'+dd;
+    const yesterday = yyyy+'-'+mm+'-'+cc;
+    console.log(data[yesterday])
+    console.log(yesterday)
+    //pull information from JSON
+    let newStock = [];
+    const yesterdayData = getOpenClose(data[yesterday]);
+    const todayData = getOpenClose(data[today]);
+
+    console.log(yesterdayData, todayData);
+}
+
+function getOpenClose(obj){
+    // console.log(obj)
+    return {
+        open: obj["1. open"],
+        close: obj["4. close"]
+    };
+}
+
 app.listen(3000, function() {
-    console.log('Listening on port 3000');
+    console.log('8:00pm');
 });
